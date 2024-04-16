2cafaa6d7cf168a6b5716fe187e9fdb9cc62cadb
siddartha <siddartha28105@gmail.com>
2024-04-16 23:16:59 +0530

    shuffle the code

---
+++ shuffle.py
@@ -0,0 +1,12 @@
+import random
+print(random.random())
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randrange(1,100))
+print(random.randrange(1,10,2))
+print(random.randrange(0,101,2))
+number=[12,23,45,67,65,43]
+random.shuffle(number)
+print(number)
+random.shuffle(number)
+print(number)
