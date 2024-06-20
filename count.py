086640de43a1414ba39fe1ba3ad537fcd56c4efc
siddartha <siddartha28105@gmail.com>
2024-06-20 11:25:29 +0530

    odd even count

---
+++ count.py
@@ -0,0 +1,11 @@
+#8)given an array, count number of even and odd and print odd and even count 
+array=[1,2,3,4,5]
+ecount=0
+ocount=0
+for i in array:
+    if i%2==0:
+        ecount=ecount+1
+    else:
+        ocount=ocount+1
+print("evencount:",ecount)
+print("oddcount:",ocount)
